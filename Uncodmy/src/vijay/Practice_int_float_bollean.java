803c7d4fdd82aaaae50683232aed4d22f4b4aa99
brijeshpal <brijeshkumarpbh761@gmail.com>
2024-02-22 14:50:50 +0530

    vijay

---
+++ Uncodmy/src/vijay/Practice_int_float_bollean.java
@@ -0,0 +1,28 @@
+package vijay;
+
+public class Practice_int_float_bollean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//int x= 11;
+		//		System.out.println(x);
+		//		System.out.println("The value of x is:- "+x);
+				
+				
+			//	float x = 11;
+			//	System.out.println("The value of x is: "+x );
+				
+			//	boolean legal= true;
+			//	boolean inlegal= false;
+			//	System.out.println("The statement is: "+legal);
+				
+				
+				String name="Brijesh pal";
+				System.out.println("The full name is:"+name);
+	}
+	
+
+}
